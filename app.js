7ba3c10ca540e59807c3e20322092ae97a8444b3
Laura González <lawr@lauragonzalez.cc>
2016-10-11 12:57:24 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+const commandLineArgs = require('command-line-args');
+const randomEmoji = require('random-emoji');
+const options = commandLineArgs([{
+	name: 'text',
+	type: String,
+	multiple: true,
+	defaultOption: true
+}]);
+
+const importantize = function(text) {
+
+	if(!text) {
+		throw 'Text argument is required'
+	}
+	if(typeof text !== 'string' && typeof text !== 'number') {
+		throw 'Text must be a string, is '+(typeof text)
+	}
+
+	const emoji = randomEmoji.random({count:2});
+
+	const returnable =
+		emoji[0].character
+		+ ' '
+		+ text.toUpperCase().split('').join(' ')
+		+ ' '
+		+ emoji[1].character
+	;
+
+	return returnable;
+}
+
+if(require.main === module) {
+	if(!options.text) {
+		throw 'Text argument is required'
+	}
+	console.log(importantize(options.text.join(' ')));
+}
+
+module.exports = importantize;
